5e0369a58ded3c8d06497e8c5f483cac6f3ff12b
barisaytekin <baris.aytekin91@gmail.com>
2023-07-14 17:27:19 +0300

    Section 3.1: String Equality completed.

--- app/src/main/java/com/barisaytekin/chapter1/MainActivity.kt
+++ app/src/main/java/com/barisaytekin/chapter1/MainActivity.kt
@@ -12,5 +12,25 @@ class MainActivity : AppCompatActivity() {
         val str1 = "Hello, World!"
         val str2 = "Hello," + " World!"
         println(str1 == str2)
+
+        //
+        val str1x = """ |Hello, World!""".trimMargin()   // adresi str1
+
+        val str2x = """ #Hello, World!""".trimMargin("#") // adres str2
+
+        val str3x = str1x  // str3 = str1 adresine sahip
+
+        println(str1x)
+        println(str2x)
+
+        println(str1x == str2x) // Prints true
+        println(str1x === str2x) // Prints false
+        println(str1x === str3x) // Prints true
+
+
+
+
+
+
     }
 }
